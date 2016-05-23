5715a59183c3d3878b4909b71158ae60582d2847
ronald <ronald@squint.nl>
2016-05-23 14:34:31 +0200

    minor changes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,20 +1,43 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        $links = \App\Link::all();
+        return view('welcome', compact('links'));
+    });
+
+    Route::get('/submit', function () {
+        return view('submit');
+    });
+
+    Route::post('/submit', function(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'url' => 'required|max:255',
+            'description' => 'required|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $link = new \App\Link;
+        $link->title = $request->title;
+        $link->url = $request->url;
+        $link->description = $request->description;
+        $link->save();
+
+        return redirect('/');
+    });
+
 });
 
-Route::auth();
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
 
-Route::get('/home', 'HomeController@index');
+    Route::get('/home', 'HomeController@index');
+});
